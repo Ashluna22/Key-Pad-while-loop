bfa8933d33b68b3a47e1155711686f2ac3094479
Ashluna22 <ashleyvera032@gmail.com>
2024-08-14 02:22:27 -0400

    first push

---
+++ key-pad-while-loop/key-pad-while-loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace key_pad_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueProgram = true;
+            string correctCode = "13579";
+
+            while (continueProgram)
+            {
+                Console.WriteLine("Hello, Please enter key code: ");
+                string code = Console.ReadLine();
+
+                if (code == correctCode)
+                {
+                    continueProgram = false;
+                    Console.WriteLine("Hello welcome home!");
+                }
+            }
+        }
+    }
+}
